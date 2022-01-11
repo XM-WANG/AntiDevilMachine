5d1373bca985c855f2c656e092e5a0dbf9e53288
XIAOYEJIAYOU <luyao.ye@outlook.com>
2022-01-11 10:47:39 +0800

    update for 3090

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import torch
 from adm import generateBlock, listenEmptyGPU
+import time
+import argparse
 
 
 if __name__ == "__main__":
@@ -7,11 +9,18 @@ if __name__ == "__main__":
     1. Replace business code part with your code.
     2. Otherwise it would generate a 5000M tensor to block the GPU.
     '''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--block_size', type=int, default=10000)
+    args = parser.parse_args()
+
     idx = listenEmptyGPU()
     device = torch.device(f"cuda:{idx}")
     
+    block_size = args.block_size
+
     ### business code ###
-    block = generateBlock(8000)
+    block = generateBlock(block_size)
     block.to(device)
     while True:
         time.sleep(1)
