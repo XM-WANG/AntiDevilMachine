14da7b8609827566aaadc24ad344bb48fd118d5c
XIAOYEJIAYOU <luyao.ye@outlook.com>
2022-01-26 12:35:15 +0800

    add a function to block specific gpu

--- block_gpu.py
+++ block_gpu.py
@@ -1,8 +1,7 @@
 import torch
-from adm import generateBlock, listenEmptyGPU
+from adm import generateBlock
 import time
 import argparse
-
 
 if __name__ == "__main__":
     '''
@@ -11,17 +10,17 @@ if __name__ == "__main__":
     '''
 
     parser = argparse.ArgumentParser()
+    parser.add_argument('-id', '--gpu_id', type=int, default=0)
     parser.add_argument('-s', '--block_size', type=int, default=22000)
     args = parser.parse_args()
-
-    idx = listenEmptyGPU()
-    device = torch.device(f"cuda:{idx}")
     
+    idx =  args.gpu_id
     block_size = args.block_size
+    
+    device = torch.device(f"cuda:{idx}")
 
     ### business code ###
     block = generateBlock(block_size)
     block.to(device)
     while True:
         time.sleep(1)
-    #####################
